b8ca2a569fa8c1be160c63aacd1225469125661e
Justin Rojas <justinmendes1612@gmail.com>
2026-08-15 19:00:02 -0600

    Separacion de datos

---
+++ CircleApp.Data/Models/Post.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CircleApp.Data.Models
+{
+    public class Post
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Content { get; set; }
+        public string? ImageUrl { get; set; }
+        public int NrOfReports { get; set; }
+      //  public bool IsPrivate { get; set; }
+        public DateTime DateCreated { get; set; }
+        public DateTime DateUpdated { get; set; }
+       // public bool IsDeleted { get; set; }
+    }
+}
--- CircleApp/Controllers/HomeController.cs
+++ CircleApp/Controllers/HomeController.cs
@@ -1,4 +1,4 @@
-using CircleApp.Models;
+
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
