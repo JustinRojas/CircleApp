f9541b31aa3108314046de5c6253df468e608c8e
Justin Rojas <justinmendes1612@gmail.com>
2026-08-16 11:24:31 -0600

    Relacion User-Post

--- CircleApp.Data/Models/User.cs
+++ CircleApp.Data/Models/User.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +9,12 @@ namespace CircleApp.Data.Models
 {
     public class User
     {
+        [Key]
         public int Id { get; set; }
         public string FullName { get; set; }
         public string? ProfilePictureUrl { get; set; }
+
+        //Navigation Property
+        public ICollection<Post> Posts { get; set; } = new List<Post>();
     }
 }
